350b3b1b56783be053648c30cac9c0351239ef7b
osaid <13340411+OmaymaS@users.noreply.github.com>
2022-03-04 15:32:31 +0100

    add exception

--- python_misc/parallel_download.py
+++ python_misc/parallel_download.py
@@ -1,13 +1,14 @@
 from itertools import repeat
 import concurrent.futures
 import wget
+import logging
 
 ## define download function
 def download_image(image_url, output_dir):
     try:
         wget.download(image_url, out = output_dir)
-    except:
-        print(f'Issue while downloading {image_url}')
+    except Exception as e:
+       logging.error(f'Error while downloading {image_url}, Error: {e}')
 
 ## define output dir and list of urls to download images   
 IMG_DIR = 'temp' ### output dir
