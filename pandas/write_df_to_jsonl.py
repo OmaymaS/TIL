a66a1c01862749d109d76bcd439580ec3580f4ce
osaid <13340411+OmaymaS@users.noreply.github.com>
2023-03-27 16:39:59 +0200

    modify example

--- pandas/write_df_to_jsonl.py
+++ pandas/write_df_to_jsonl.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import json
 
-df = pd.DataFrame({'url': ['https://page.com/p1', 'https://page.com/p2'],
+sample_data = pd.DataFrame({'url': ['https://page.com/p1', 'https://page.com/p2'],
                    'id': ['10010', '93945']})
 
 
-with open('output.jsonl', 'w') as f:
+with open('sample_data.jsonl', 'w') as f:
     for _, row in df.iterrows():
         json.dump(row.to_dict(), f, ensure_ascii=False)
         _ = f.write('\n')
